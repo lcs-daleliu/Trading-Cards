edb283f3dce7b9c6563db0f7a47625f9b0907b81
Dale Liu <YLiu@lcs.on.ca>
2024-01-21 14:37:24 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by Yiren LIU on 2024-01-21.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
